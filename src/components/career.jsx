84b9ae84c3677562da321afb66e7fb0c84fcd5d3
Chayma Rouaa <chayma.rouaa@gmail.com>
2026-08-22 06:16:20 +0200

    Configure GitHub Pages deployment

--- src/components/career.jsx
+++ src/components/career.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { motion } from "motion/react"
-import SectionTitle from "./SectionTitle"
+import SectionTitle from "./sectiontitle"
 
 const careerGoals = [
   {
